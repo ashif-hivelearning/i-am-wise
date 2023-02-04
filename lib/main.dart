fac09c3ec0fd0c257156881a63bc3e130bcf5fd8
Ashif Mujtoba <nmujtoba@gmail.com>
2023-02-04 12:30:52 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(WiseApp());
+
+class WiseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuestionPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionPage extends StatefulWidget {
+  @override
+  QuestionPageState createState() => QuestionPageState();
+}
+
+class QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Question will be added here.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Images will be added here.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here to show your right or wrong answer
+      ],
+    );
+  }
+}
---
+++ lib/resources.dart
@@ -0,0 +1,32 @@
+/*
+question1: 'Flutter can only develop mobile applications.', false,
+question2: 'Colombo is the capital of Sri Lanka.', true,
+question3: 'Cox's Bazar is the longest sea beach in the world.', true,
+*/
+
+/*
+"images/flutter.png"
+"images/colombo.jpeg"
+"images/sea-beach.jpeg"
+*/
+
+/*
+Card(
+                color: Colors.white,
+                shadowColor: Colors.grey,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "images/sea-beach.jpeg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              )
+* */
